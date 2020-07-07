2b2ad97bdda25825191ae0ecb480f6aae0f161f8
Tejaswi Vemulapati <tejaswi.vemulapati@gmail.com>
2020-07-07 16:34:51 -0700

    Add files via upload

--- BLE/BLE/CoreBluetoothManager.swift
+++ BLE/BLE/CoreBluetoothManager.swift
@@ -20,7 +20,6 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
     var data = Data()
     
     // Peripheral Data
-    var peripheralManagerBLE: CBPeripheralManager!
     
     var transferCharacteristicBLE: CBMutableCharacteristic?
     var connectedCentral: CBCentral?
@@ -30,7 +29,6 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
     
     override init() {
         centralManagerBLE = CBCentralManager()
-        peripheralManagerBLE = CBPeripheralManager()
         serviceUUID = CBUUID(string: "E20A39F4-73F5-4BC4-A12F-17D1AD07A961")   //Look into Bluetooth Specs
         characteristicsUUID = CBUUID(string: "08590F7E-DB05-467E-8757-72F6FAEB13D4")
         print("BLE Manager Initialized")
@@ -41,14 +39,13 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
         
         let connectedPeripherals: [CBPeripheral] = (centralManagerBLE.retrieveConnectedPeripherals(withServices: [serviceUUID]))
         
-        print("Found connected Peripherals with transfer service: %@", connectedPeripherals)
+        print("Found connected Peripherals with transfer service: %@", connectedPeripherals.first)
         
         if let connectedPeripheral = connectedPeripherals.last {
             print("Connecting to peripheral %@", connectedPeripheral)
             self.discoveredPeripheral = connectedPeripheral
             centralManagerBLE.connect(connectedPeripheral, options: nil)
         } else {
-            // We were not connected to our counterpart, so start scanning
             centralManagerBLE.scanForPeripherals(withServices: [serviceUUID],
                                               options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
         }
@@ -191,15 +188,10 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
         }
     }
     
-    // Peripheral Delegate
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?){
-        /*
-        print("Peripheral Delegate Initiated")
-        if let service = peripheral.services?.first(where: { $0.uuid == serviceUUID}){
-            peripheral.discoverCharacteristics([characteristicsUUID], for: service) // For MouseCharacteristic UUID
-            print("Discovered Characteristics")
-        }
- */
+    /*
+     *  The Transfer Service was discovered
+     */
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         if let error = error {
             print("Error discovering services: %s", error.localizedDescription)
             cleanup()
@@ -215,13 +207,12 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
         }
     }
     
-    func peripheral(_ peripheral:CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?){
-        /*
-        if let characteristic = service.characteristics?.first(where: {$0.uuid == characteristicsUUID}) {
-            peripheral.setNotifyValue(true, for: characteristic)
-        }
-        print("Peripheral func invoked")
- */
+    /*
+     *  The Transfer characteristic was discovered.
+     *  Once this has been found, we want to subscribe to it, which lets the peripheral know we want the data it contains
+     */
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        // Deal with errors (if any).
         if let error = error {
             print("Error discovering characteristics: %s", error.localizedDescription)
             cleanup()
@@ -235,9 +226,15 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
             transferCharacteristic = characteristic
             peripheral.setNotifyValue(true, for: characteristic)
         }
+        
+        // Once this is complete, we just need to wait for the data to come in.
     }
     
-    func peripheral(_ peripheral:CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?){
+    /*
+     *   This callback lets us know more data has arrived via notification on the characteristic
+     */
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        // Deal with errors (if any)
         if let error = error {
             print("Error discovering characteristics: %s", error.localizedDescription)
             cleanup()
@@ -265,47 +262,84 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
             data.append(characteristicData)
         }
     }
+    
+    /*
+     *  The peripheral letting us know whether our subscribe/unsubscribe happened or not
+     */
     func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
         // Deal with errors (if any)
         if let error = error {
             print("Error changing notification state: %s", error.localizedDescription)
+            return
         }
-        else{
-            // Exit if it's not the transfer characteristic
-            guard characteristic.uuid == characteristicsUUID else { return }
-            
-            if characteristic.isNotifying {
-                // Notification has started
-                print("Notification began on %@", characteristic)
-            } else {
-                // Notification has stopped, so disconnect from the peripheral
-                print("Notification stopped on %@. Disconnecting", characteristic)
-                cleanup()
-            }
+        
+        // Exit if it's not the transfer characteristic
+        guard characteristic.uuid == characteristicsUUID else { return }
+        
+        if characteristic.isNotifying {
+            // Notification has started
+            print("Notification began on %@", characteristic)
+        } else {
+            // Notification has stopped, so disconnect from the peripheral
+            print("Notification stopped on %@. Disconnecting", characteristic)
+            cleanup()
         }
+        
     }
+    
+    /*
+     *  This is called when peripheral is ready to accept more data when using write without response
+     */
     func peripheralIsReady(toSendWriteWithoutResponse peripheral: CBPeripheral) {
         print("Peripheral is ready, send data")
         writeData()
     }
     
+}
+
+// NEW CLASS
+
+class PeripheralManagerBLE: NSObject, CBPeripheralDelegate {
+    
+    var serviceUUID:CBUUID!  //Look into Bluetooth Specs
+    var characteristicsUUID:CBUUID!
+    
+    var peripheralManager: CBPeripheralManager!
+    
+    var transferCharacteristic: CBMutableCharacteristic?
+    var connectedCentral: CBCentral?
+    var dataToSend = Data()
+    var sendDataIndex: Int = 0
+
+    var isAdvertising:Bool!
+    override init() {
+        peripheralManager = CBPeripheralManager()
+        serviceUUID = CBUUID(string: "E20A39F4-73F5-4BC4-A12F-17D1AD07A961")   //Look into Bluetooth Specs
+        characteristicsUUID = CBUUID(string: "08590F7E-DB05-467E-8757-72F6FAEB13D4")
+        isAdvertising = false
+        print("Peripheral Manager Initialized")
+    }
+
+    static var sendingEOM = false
+    
     private func sendData() {
         
-        guard let transferCharacteristic = transferCharacteristicBLE else {
+        guard let transferCharacteristic = transferCharacteristic else {
             return
         }
         
         // First up, check if we're meant to be sending an EOM
-        if sendingEOM {
+        if PeripheralManagerBLE.sendingEOM {
             // send it
-            let didSend = peripheralManagerBLE.updateValue("EOM".data(using: .utf8)!, for: transferCharacteristic, onSubscribedCentrals: nil)
+            let didSend = peripheralManager.updateValue("EOM".data(using: .utf8)!, for: transferCharacteristic, onSubscribedCentrals: nil)
             // Did it send?
             if didSend {
                 // It did, so mark it as sent
-                sendingEOM = false
+                PeripheralManagerBLE.sendingEOM = false
                 print("Sent: EOM")
             }
             // It didn't send, so we'll exit and wait for peripheralManagerIsReadyToUpdateSubscribers to call sendData again
+            return
         }
         
         // We're not sending an EOM, so we're sending data
@@ -329,7 +363,7 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
             let chunk = dataToSend.subdata(in: sendDataIndex..<(sendDataIndex + amountToSend))
             
             // Send it
-            didSend = peripheralManagerBLE.updateValue(chunk, for: transferCharacteristic, onSubscribedCentrals: nil)
+            didSend = peripheralManager.updateValue(chunk, for: transferCharacteristic, onSubscribedCentrals: nil)
             
             // If it didn't work, drop out and wait for the callback
             if !didSend {
@@ -346,21 +380,22 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
                 // It was - send an EOM
                 
                 // Set this so if the send fails, we'll send it next time
-                sendingEOM = true
+                PeripheralManagerBLE.sendingEOM = true
                 
                 //Send it
-                let eomSent = peripheralManagerBLE.updateValue("EOM".data(using: .utf8)!,
+                let eomSent = peripheralManager.updateValue("EOM".data(using: .utf8)!,
                                                             for: transferCharacteristic, onSubscribedCentrals: nil)
                 
                 if eomSent {
                     // It sent; we're all done
-                    sendingEOM = false
+                    PeripheralManagerBLE.sendingEOM = false
                     print("Sent: EOM")
                 }
                 return
             }
         }
     }
+    
     private func setupPeripheral() {
         
         // Build our service.
@@ -372,53 +407,81 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
                                                              permissions: [.readable, .writeable])
         
         // Create a service from the characteristic.
-        let transferService = CBMutableService(type: characteristicsUUID, primary: true)
+        let transferService = CBMutableService(type: serviceUUID, primary: true)
         
         // Add the characteristic to the service.
         transferService.characteristics = [transferCharacteristic]
         
         // And add it to the peripheral manager.
-        peripheralManagerBLE.add(transferService)
+        peripheralManager.add(transferService)
         
         // Save the characteristic for later.
-        transferCharacteristicBLE = transferCharacteristic
+        self.transferCharacteristic = transferCharacteristic
         
     }
+}
+
+extension PeripheralManagerBLE: CBPeripheralManagerDelegate {
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        peripheralManagerDidUpdateState(peripheral, strValue: "Something")
+    }
+    
+    // implementations of the CBPeripheralManagerDelegate methods
+    
+    /*
+     *  Required protocol method.  A full app should take care of all the possible states,
+     *  but we're just waiting for to know when the CBPeripheralManager is ready
+     *
+     *  Starting from iOS 13.0, if the state is CBManagerStateUnauthorized, you
+     *  are also required to check for the authorization state of the peripheral to ensure that
+     *  your app is allowed to use bluetooth
+     */
     internal func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager, strValue: String){
         
-        isAdvertisingBLE = peripheral.state == .poweredOn
+        isAdvertising = peripheral.state == .poweredOn
         
         switch peripheral.state {
         case .poweredOn:
             // ... so start working with the peripheral
-            print("CBManager is powered on")
+            print("CBManager is powered on. Setting Up Peripheral for Transfer Service.")
             setupPeripheral()
         case .poweredOff:
             print("CBManager is not powered on")
+            // In a real app, you'd deal with all the states accordingly
             break
         case .resetting:
             print("CBManager is resetting")
+            // In a real app, you'd deal with all the states accordingly
             break
         case .unauthorized:
+            // In a real app, you'd deal with all the states accordingly
             if #available(iOS 13.0, *){
                 print("Available")
             }
         case .unknown:
             print("CBManager state is unknown")
+            // In a real app, you'd deal with all the states accordingly
             break
         case .unsupported:
             print("Bluetooth is not supported on this device")
+            // In a real app, you'd deal with all the states accordingly
             break
         @unknown default:
             print("A previously unknown peripheral manager state occurred")
+            // In a real app, you'd deal with yet unknown cases that might occur in the future
             break
         }
+        print("Updated Peripheral State")
     }
+    
+    /*
+     *  Catch when someone subscribes to our characteristic, then start sending them data
+     */
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
         print("Central subscribed to characteristic")
         
         // Get the data
-        dataToSend = "Hi! This is a Test!".data(using: .utf8)!
+        dataToSend = "".data(using: .utf8)!
         
         // Reset the index
         sendDataIndex = 0
@@ -429,14 +492,27 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
         // Start sending
         sendData()
     }
+    
+    /*
+     *  Recognize when the central unsubscribes
+     */
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
         print("Central unsubscribed from characteristic")
         connectedCentral = nil
     }
+    
+    /*
+     *  This callback comes in when the PeripheralManager is ready to send the next chunk of data.
+     *  This is to ensure that packets will arrive in the order they are sent
+     */
     func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
         // Start sending again
         sendData()
     }
+    
+    /*
+     * This callback comes in when the PeripheralManager received write to characteristics
+     */
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
         for aRequest in requests {
             guard let requestValue = aRequest.value,
@@ -445,8 +521,8 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
             }
             
             print("Received write request of %d bytes: %s", requestValue.count, stringFromData)
-           print(stringFromData)
+            print(stringFromData)
         }
     }
-    
 }
+
--- BLE/BLE/ViewController.swift
+++ BLE/BLE/ViewController.swift
@@ -4,6 +4,7 @@ import CoreBluetooth
 class ViewController: NSViewController{
     
     var bleManager:BLEManager!
+    var blePeripheralManager:PeripheralManagerBLE!
     
     @IBOutlet weak var adButton: NSButton!
     @IBOutlet weak var modeButton: NSButton!
@@ -14,37 +15,43 @@ class ViewController: NSViewController{
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        bleManager = BLEManager()
-        bleManager.centralManagerBLE.delegate = bleManager
         isCentral = false
-        bleManager.isAdvertisingBLE = false
     }
     override func viewWillDisappear() {
         bleManager.centralManagerBLE.stopScan()
         print("Scanning stopped")
         bleManager.data.removeAll(keepingCapacity: false)
-        bleManager.peripheralManagerBLE.stopAdvertising()
+        blePeripheralManager.peripheralManager.stopAdvertising()
         super.viewWillDisappear()
     }
     
     @IBAction func adChange(_ sender: NSButton) {
-        bleManager.isAdvertisingBLE = !bleManager.isAdvertisingBLE
-        if(bleManager.isAdvertisingBLE){
-    bleManager.peripheralManagerBLE.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [bleManager.serviceUUID]])
-            print("Is Advertising")
+        if(isCentral){
+            print("Advertising Does Not Apply In Central Mode")
         }
         else{
-            bleManager.peripheralManagerBLE.stopAdvertising()
-            print("Is Not Advertising")
+            blePeripheralManager.isAdvertising = !blePeripheralManager.isAdvertising
+            if(blePeripheralManager.isAdvertising){
+        blePeripheralManager.peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [blePeripheralManager.serviceUUID]])
+                print("Is Advertising")
+            }
+            else{
+                blePeripheralManager.peripheralManager.stopAdvertising()
+                print("Is Not Advertising")
+            }
         }
     }
     
     @IBAction func changeMode(_ sender: NSButton) {
         isCentral = !isCentral
         if(isCentral){
+            bleManager = BLEManager()
+            bleManager.centralManagerBLE.delegate = bleManager
             print("Central Mode")
         }
         else{
+            blePeripheralManager = PeripheralManagerBLE()
+            blePeripheralManager.peripheralManager.delegate = blePeripheralManager
             print("Peripheral Mode")
         }
     }
